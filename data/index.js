60eb7ff125ca65bfa16bd32301a5bb16c8e0f145
Prophet-Bestman <bestiproph@gmail.com>
2022-01-15 21:55:24 +0100

    Unlinked nav

--- data/index.js
+++ data/index.js
@@ -532,9 +532,9 @@ export const ServiceOneData = [
   },
 ];
 
-import projectImage1 from "@/images/raz/coza.jpg";
-import projectImage2 from "@/images/raz/mayorkun.jpg";
-import projectImage3 from "@/images/raz/hair_flip.jpg";
+import projectImage1 from "@/images/raz/coza.JPG";
+import projectImage2 from "@/images/raz/mayorkun.JPG";
+import projectImage3 from "@/images/raz/hair_flip.JPG";
 import projectImage4 from "@/images/project-1-4.jpg";
 import projectImage5 from "@/images/project-1-5.jpg";
 import projectImage6 from "@/images/project-1-6.jpg";
